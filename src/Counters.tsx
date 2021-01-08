39d09fd8a0eaaf869ff5f6dab6e010e3cac7eaba
Larisa-Yagodina <cheeseroll@mail.ru>
2021-01-08 14:09:36 +0300

    used boolean

--- src/Counters.tsx
+++ src/Counters.tsx
@@ -11,20 +11,21 @@ function Menu(props: IProps) {
     return (
         <div>
             <h2>Counters</h2>
-
-            {props.counters.map((el, i) =>
-                <p key={i}>
-                    <button
-                        onClick={() => props.plusMinusCounters(i, -1)}
-                    > -1
-                    </button>
-                    {' '}{el}{' '}
-                    <button
-                        onClick={() => props.plusMinusCounters(i, 1)}
-                    > 1
-                    </button>
-                </p>
-            )}
+            {props.isOpen && <>
+                {props.counters.map((el, i) =>
+                    <p key={i}>
+                        <button
+                            onClick={() => props.plusMinusCounters(i, -1)}
+                        > -1
+                        </button>
+                        {' '}{el}{' '}
+                        <button
+                            onClick={() => props.plusMinusCounters(i, 1)}
+                        > 1
+                        </button>
+                    </p>
+                )}
+            </>}
         </div>
     );
 }
